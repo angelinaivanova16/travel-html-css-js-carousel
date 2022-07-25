3234b59ef76ec5adcac46dc1b99600ec0f99800a
Angelina Ivanova <angelinaivanova16@gmail.com>
2022-07-26 02:35:08 +0300

    feat: finish the slider

--- travel/script.js
+++ travel/script.js
@@ -2,7 +2,6 @@
 const burgerIcon = document.querySelector('.hamburger');
 const links = document.querySelector('.navig');
 const n = document.querySelector('.plan');
-// const k = document.querySelector('.destinations');
 
 function toggleMenu() {
   burger.classList.toggle('is-active');
@@ -17,7 +16,8 @@ function hideMenu() {
 burgerIcon.addEventListener('click', toggleMenu);
 links.addEventListener('click', hideMenu);
 n.addEventListener('click', hideMenu);
-// k.addEventListener('click', hideMenu);
+
+
 
 
 // slider
@@ -26,43 +26,64 @@ let active = document.querySelector("#slide-active");
 let right = document.querySelector("#slide-right");
 const CAROUSEL = document.querySelector("#carousel")
 
+let ellipse1 = document.querySelector("#ell1");
+let ellipse2 = document.querySelector("#ell2");
+let ellipse3 = document.querySelector("#ell3");
+
+
 function moveLeft() {
   CAROUSEL.classList.add("transition-left");
-  left.removeEventListener("click", moveLeft);
-  right.removeEventListener("click", moveRight);
+  active.classList.add("r");
+  ellipse1.classList.add("ellipse-main");
+  ellipse2.classList.remove("ellipse-main");
+  ellipse2.classList.add("ellipse");
 }
 function moveRight() {
   CAROUSEL.classList.add("transition-right");
-  left.removeEventListener("click", moveLeft);
-  right.removeEventListener("click", moveRight);
+  active.classList.add("l");
+  ellipse3.classList.add("ellipse-main");
+  ellipse2.classList.remove("ellipse-main");
+  ellipse2.classList.add("ellipse");
+}
+function moveVarious() {
+  if (active.classList.contains('l')) {
+    CAROUSEL.classList.remove("transition-right");
+    CAROUSEL.classList.remove("transition-left");
+    CAROUSEL.classList.add("slide2");
+    ellipse2.classList.add("ellipse-main");
+    ellipse3.classList.remove("ellipse-main");
+    ellipse3.classList.add("ellipse");
+    ellipse1.classList.remove("ellipse-main");
+  } else {
+    CAROUSEL.classList.remove("transition-left");
+    CAROUSEL.classList.add("slide2");
+    ellipse2.classList.add("ellipse-main");
+    ellipse1.classList.remove("ellipse-main");
+    ellipse1.classList.add("ellipse");
+  }
 }
 
 left.addEventListener("click", moveLeft);
 right.addEventListener("click", moveRight);
+active.addEventListener("click", moveVarious);
 
 
 
 
-console.log(`76 баллов.
+
+console.log(`50 баллов.
       
-1. Вёрстка соответствует макету. Ширина экрана 390px (39 баллов).
-* Блок header 
-* Секция preview 
-* Секция steps 
-* Секция destinations 
-* Секция stories - с макетом расходятся изображения
-* Блок footer 
+1. Слайдер изображений в секции destinations (50 баллов).
+* На десктоп варианте при клике на урезанную картинку слева или справа изображение меняется по принципу карусели
+Слайдер может быть как конечным так и бесконечным - данный критерий не должен влиять на оценку +20
+*  Три точки внизу отображают "номер слайда",
+ то есть каждому слайду соответствует свой кружочек,
+который становится активным при нахождении соответствующего ему слайда в центре. +20
+*  Анимации плавного перемещения для слайдера +10
 
-2. Ни на одном из разрешений до 320px включительно не появляется горизонтальная
- полоса прокрутки. Весь контент страницы при этом сохраняется: не обрезается и не удаляется (15 баллов).
+2. 
 
-3. На ширине экрана 390рх и меньше реализовано адаптивное меню (22 балла).
-при ширине страницы 390рх панель навигации скрывается, появляется бургер-иконка 
-* при нажатии на бургер-иконку плавно появляется адаптивное меню 
-* адаптивное меню соответствует макету 
-* при нажатии на крестик адаптивное меню плавно скрывается уезжая за экран 
-* ссылки в адаптивном меню работают, обеспечивая плавную прокрутку по якорям (все, кроме Account, она пока просто закрывает меню) 
-* при клике по ссылке в адаптивном меню адаптивное меню плавно скрывается, также скрытие меню происходит если сделать клик вне данного окна.
+3.
 
-Итого: 76 баллов.
+Итого: 50 баллов.
 `);
